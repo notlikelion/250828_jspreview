118b321a4cd77c68fcd62088e5e5a0c56a93a6f9
notlikelion <notlikelion@gmail.com>
2025-08-28 10:18:15 +0900

    Initial commit

---
+++ src/main/java/com/example/jspreview/JspreviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.jspreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspreviewApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>모두 안녕!</title>
+</head>
+<body>
+    <h1>반가워요!</h1>
+</body>
+</html>
