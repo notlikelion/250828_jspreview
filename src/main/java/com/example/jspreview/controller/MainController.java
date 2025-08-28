679b050e4e8cb19ad22e41f66d5baafe24e8cb9f
notlikelion <notlikelion@gmail.com>
2025-08-28 11:19:49 +0900

    푸시푸시~

--- src/main/java/com/example/jspreview/controller/MainController.java
+++ src/main/java/com/example/jspreview/controller/MainController.java
@@ -1,6 +1,10 @@
 package com.example.jspreview.controller;
 
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // 1. 컴포넌트 스캔에 'Controller'로 잡히게 만들어야한다
@@ -8,9 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 // 3. 어떤 jsp로 응답해줄까 resolver.
 @Controller
 public class MainController {
-    // ?
+    @Value("${google.apikey}")
+    private String apiKey;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        System.out.println(apiKey);
+        Client client = Client.builder()
+                .apiKey(apiKey)
+                .build();
+        GenerateContentResponse response = client.models.generateContent(
+                "gemini-2.5-flash-lite", "오늘 점심 뭐 먹지?", null);
+        model.addAttribute("text",  response.text());
+
         return "index"; // ? WEB-INF 보안경로
     }
+
 }
