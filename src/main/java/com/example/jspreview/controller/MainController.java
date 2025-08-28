760b3ac34e7eb69350d9686955e60fa0272433a3
notlikelion <notlikelion@gmail.com>
2025-08-28 11:34:43 +0900

    푸시푸시~

--- src/main/java/com/example/jspreview/controller/MainController.java
+++ src/main/java/com/example/jspreview/controller/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // 1. 컴포넌트 스캔에 'Controller'로 잡히게 만들어야한다
 // 2. Mapping 설정 -> url 대응
@@ -17,15 +19,28 @@ public class MainController {
 
     @GetMapping("/")
     public String index(Model model) {
-        System.out.println(apiKey);
-        Client client = Client.builder()
-                .apiKey(apiKey)
-                .build();
-        GenerateContentResponse response = client.models.generateContent(
-                "gemini-2.5-flash-lite", "오늘 점심 뭐 먹지?", null);
-        model.addAttribute("text",  response.text());
+//        System.out.println(apiKey);
+//        Client client = Client.builder()
+//                .apiKey(apiKey)
+//                .build();
+//        GenerateContentResponse response = client.models.generateContent(
+//                "gemini-2.5-flash-lite", "다이어트를 위한 점심 식단을 '마크다운 같은 꾸미는 문법 없이' 평문으로 하되, 전문가 느낌이 나게 작성해줘. 100자 이내로.", null);
+//        model.addAttribute("text",  response.text());
+        model.addAttribute("text", "뭐든지 물어보세요!");
 
         return "index"; // ? WEB-INF 보안경로
     }
 
-}
+//    @PostMapping("/")
+    @PostMapping
+    public String index(Model model,
+                        @RequestParam("question") String question) {
+        Client client = Client.builder()
+                .apiKey(apiKey)
+                .build();
+        GenerateContentResponse response = client.models.generateContent(
+                "gemini-2.5-flash-lite", question, null);
+        model.addAttribute("text", response.text());
+        return "index"; // 경로는 같은데 전달된 데이터가 다른 jsp
+        }
+    }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -5,6 +5,10 @@
 </head>
 <body>
     <h1>반가워요!</h1>
+    <form method="post">
+        <input name="question" placeholder="질문을 입력해보세요">
+        <button>질문하기</button>
+    </form>
     <p>
         <%= request.getAttribute("text") %>
     </p>
